7818f02a59dd185ebec9ca11c6bace7f7e834ec8
Alhexx02 <103387346+Alhexx02@users.noreply.github.com>
2022-06-03 16:28:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'Hiyabo'
+        tl_admin_user = 'Shadowalh'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -560,7 +560,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5206363628:AAGP-Vpv5zLqEdBMlj-ZK5CDiCpIoXDnVPg'
+    bot_token = '5471631481:AAEjvJ9iMRrFnU9rxfyi4PbcZHoG789idZw'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
